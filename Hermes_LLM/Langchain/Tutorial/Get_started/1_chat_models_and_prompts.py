c3b8124833d24847e89b0ee619aafbda76c9b592
Henry Zhang <2376817023@qq.com>
2025-03-11 00:13:18 +0800

    feat: langchain；tutorial；get started；chat models and prompts

---
+++ Hermes_LLM/Langchain/Tutorial/Get_started/1_chat_models_and_prompts.py
@@ -0,0 +1,47 @@
+"""
+Chat models and prompts: Build a simple LLM application with prompt templates and chat models.
+"""
+from langchain_ollama.chat_models import ChatOllama
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate
+
+# 1. Using Language Models
+llm_model = ChatOllama(
+    model="deepseek-r1:14b",
+    temperature=0
+)
+messages = [
+    SystemMessage("Translate the following from English into Italian"),
+    HumanMessage("hi!"),
+]
+
+# 1.1 messages invoking
+response = llm_model.invoke(messages)
+print(response, type(response), sep="\n")  # <class 'langchain_core.messages.ai.AIMessage'>
+
+# 1.2 other ways to invoke
+print(llm_model.invoke("Hello"))
+print(llm_model.invoke([{"role": "user", "content": "Hello"}]))
+print(llm_model.invoke([HumanMessage("Hello")]))
+
+# 1.3 streaming ways
+for token in llm_model.stream(messages):
+    print(token.content, end="|")
+
+# 2. Prompt Templates
+print("\n", "*" * 100)
+system_template = "Translate the following from English into {language}"
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", "{text}")]
+)
+
+# 2.1 the input to this prompt template is a dictionary.
+prompt = prompt_template.invoke({"language": "Italian", "text": "hi!"})
+print(prompt)
+
+# 2.2 access the messages directly
+print(prompt.to_messages())
+
+# 2.3 invoke the chat model
+response = llm_model.invoke(prompt)
+print(response.content)
